09aa65cdac1c03e369b4d95eee93d9752102cf8b
Tn6rg3 <157599757+Tn6rg3@users.noreply.github.com>
2026-08-10 15:05:46 +0200

    Update app.js

--- game3/js/app.js
+++ game3/js/app.js
@@ -4,7 +4,7 @@
 
 const BOT_USERNAME = "cwappgame_bot";
 const WEBAPP_NAME = "cwgame";
-const APP_VERSION = "20260807.220";
+const APP_VERSION = "20260807.222";
 
 window.Telegram.WebApp.ready();
 window.Telegram.WebApp.expand();
