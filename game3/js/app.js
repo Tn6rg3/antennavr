5281be819a729c214d6feafdf6a954cb7e2d86b4
Tn6rg3 <157599757+Tn6rg3@users.noreply.github.com>
2026-08-31 18:09:17 +0200

    Add files via upload

--- game3/js/app.js
+++ game3/js/app.js
@@ -504,12 +504,6 @@ async function startApp() {
     myName = tgUser.first_name;
     myId = tgUser.id.toString();
 
-    // --- REGISTRAZIONE IDENTITÀ IMMEDIATA (Fix Permission Denied) ---
-    if (firebase.auth().currentUser) {
-        db.ref(`uid_mapping/${firebase.auth().currentUser.uid}`).set(myId)
-            .then(() => console.log("App: Identità mappata con successo."))
-            .catch(e => console.warn("App: Errore mappatura iniziale:", e));
-    }
     // --- TELEGRAM TTS FIX: Svegliamo il motore vocale ---
     if ('speechSynthesis' in window) {
         window.speechSynthesis.getVoices();
