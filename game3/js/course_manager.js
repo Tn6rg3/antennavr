1a10d94cc22134ea9a1f499bff52a1ee1efe55eb
Tn6rg3 <157599757+Tn6rg3@users.noreply.github.com>
2026-09-17 14:08:31 +0200

    Add files via upload

--- game3/js/course_manager.js
+++ game3/js/course_manager.js
@@ -708,6 +708,15 @@ window.playNextCourseGroup = function() {
     lastWordStartTime = Date.now();
 };
 
+window.getWeekNumber = function(d) {
+    if (!d) d = new Date();
+    const date = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
+    date.setUTCDate(date.getUTCDate() + 4 - (date.getUTCDay() || 7));
+    const yearStart = new Date(Date.UTC(date.getUTCFullYear(), 0, 1));
+    const weekNo = Math.ceil((((date - yearStart) / 86400000) + 1) / 7);
+    return `${date.getUTCFullYear()}-W${weekNo < 10 ? '0' + weekNo : weekNo}`;
+};
+
 window.checkWeeklyReview = function() {
     if (!window.courseData || !window.courseData.active_plan) return;
     const now = new Date();
